aee63eb0edfca42ca62e3e0bd5dd1622ee788bdf
Alexander <flint.979@gmail.com>
2023-07-15 09:50:41 +0300

    15-07-23

--- src/Task04.java
+++ src/Task04.java
@@ -8,16 +8,16 @@ public class Task04 {
         for (int i = 0; i < size*2-1; i++) {
             for (int j = 0; j < size * 2; j++) {
                 if (i<size&&(j >= size - i) && (j <= size + i)) System.out.print("*");
-                else if (i>=size && (j > i-size+1)&&(j<size*2-1-j) ) System.out.print("*");
+                else if (i>=size&& ((j > i-size+1)&&(j<size*2-1-i+size) )) System.out.print("*");
                 else System.out.print(" ");
             }
             System.out.println();
         }
         System.out.println();
-        for (int i = 0; i < size; i++) {
+        for (int i = 0; i < size*2-1; i++) {
             for (int j = 0; j < size * 2; j++) {
-                if (i == size - 1 && j > 0) System.out.print("*");
-                else if ((j == size - i) || (j == size + i)) System.out.print("*");
+                if (i<size&&(j == size - i) || (j == size + i)) System.out.print("*");
+                else if (i>=size&& ((j == i-size+2)||(j==size*2-2-i+size) )) System.out.print("*");
                 else System.out.print(" ");
             }
             System.out.println();
