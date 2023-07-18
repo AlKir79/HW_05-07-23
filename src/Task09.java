7c16a7c546a1cd5fab03157d97be74cdfff64da8
flint <flint.979@gmail.com>
2023-07-18 21:48:44 +0300

    18-07-23 (Task09)

--- src/Task09.java
+++ src/Task09.java
@@ -6,14 +6,18 @@ public class Task09 {
         System.out.println("Введите число ярусов и высоту через пробел");
         int size = in.nextInt();
         int height = in.nextInt();
+        int count = size;
         for(int k=0;k<size;k++){
             for (int i = 0; i < height; i++) {
+                for(int l=0;l<count;l++)
+                {System.out.print(" ");}
                 for (int j = 0; j < height * 2; j++) {
                     if ((j >= height - i) && (j <= height + i)) System.out.print("*");
                     else System.out.print(" ");
                 }
                 System.out.println();
             }
+            count--;
             height++;
         }
 
